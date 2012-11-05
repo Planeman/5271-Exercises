9cb61e5e185615e296b1685954f08ba8c4185081
Planeman <planemantheace@gmail.com>
2012-11-04 21:44:50 -0600

    Updated cracker to support both brute force and dictionary cracks. The brute forcer can run on multiple processes as well.

--- hw2/q2/find_auth_pair.py
+++ hw2/q2/find_auth_pair.py
@@ -5,6 +5,7 @@ import gc
 import sys
 import time
 import hashlib
+from multiprocessing import Process
 
 req_header_re = re.compile("(?P<method>GET)\s*(?P<uri>.+?)\s*HTTP/1\.1", re.M | re.I)
 
@@ -20,16 +21,28 @@ auth_resp_re = re.compile(auth_resp_raw, re.M | re.I)
 
 
 # ASCII character ranges
-numbers = range(48,58)
-cap_letters = range(65,91)
-low_letters = range(97,123)
+numbers = range(48, 58)
+cap_letters = range(65, 91)
+low_letters = range(97, 123)
+
+pass_max = 10
+pass_min = 0
+
+max_procs = 4  # Currently only applies during brute force
+
+cracked_file = "cracked_pass.log"
+
+def genPasswords(_min, _max, charset):
+    for l in range(_min, _max + 1):
+        for pw in xselections(charset, l):
+            yield ''.join(pw)
 
 def xselections(items, n):
-  if n==0: yield []
-  else:
-    for i in xrange(len(items)):
-      for ss in xselections(items, n-1):
-        yield [items[i]]+ss
+    if n == 0: yield []
+    else:
+        for i in xrange(len(items)):
+            for ss in xselections(items, n - 1):
+                yield [items[i]] + ss
 
 
 class AuthContextException(Exception):
@@ -37,139 +50,152 @@ class AuthContextException(Exception):
 
 
 class AuthContext:
-  def __init__(self, pw_generator):
-    self.realm = ""
-    self.username = ""
-    self.password = ""  # Our target
+    def __init__(self, pw_generator):
+        self.realm = ""
+        self.username = ""
+        self.password = ""  # Our target
 
-    self.qop = "auth"
-    self.server_nonce = ""
-    self.client_nonce = ""
-    self.nonce_count = ""  # nc in the http header
+        self.qop = "auth"
+        self.server_nonce = ""
+        self.client_nonce = ""
+        self.nonce_count = ""  # nc in the http header
 
-    self.request_method = "GET"
-    self.request_uri = ""
+        self.request_method = "GET"
+        self.request_uri = ""
 
-    self.client_resp = ""
+        self.client_resp = ""
 
-    self.hashtype = "MD5"
+        self.hashtype = "MD5"
 
-    self.pw_gen = pw_generator
-
-
-  def crackPassword(self, data=None, filename=None):
-    if data is None:
-      if filename is None:
-        raise AuthContextException("No data provided and filename is None")
-      else:
-        with open(filename, 'r') as f:
-          data = f.read()
-
-    self.extractServerAuthData(data)
-    self.extractClientAuthData(data)
-
-    print(str(self))
-    print("Starting password crack...")
-
-    # Using the notation from the wikipedia page here:
-    # http://en.wikipedia.org/wiki/Digest_access_authentication
-
-    ha1_template = "{0.username}:{0.realm}:{{}}".format(self)
-    ha2 = hashlib.md5("{0.request_method}:{0.request_uri}".format(self)).hexdigest()
-
-    # HA1:snonce:nc:cnonce:qop:HA2
-    resp = "{{}}:{0.server_nonce}:{0.nonce_count}:{0.client_nonce}:{0.qop}:{1}".format(self, ha2)
-    print("HA1 template = " + ha1_template)
-    print("HA2 = " + ha2)
-    print("resp template = " + resp)
-
-    # This is code from the old brute force method where we would just cruise through all
-    # various permutation.
-    #ascii_list = []
-    #for i in self.charset:
-    #  ascii_list.append(str(chr(i)))
-
-    count = 0
-    stime = time.time()
-    for pw in self.pw_gen:
-        ha1 = hashlib.md5(ha1_template.format(pw)).hexdigest()
-        full_resp = resp.format(ha1)
-        if hashlib.md5(full_resp).hexdigest() == self.client_resp:
-          print("Found password: {}".format(pw))
-          return
-
-        count += 1
-        if count % 10000 == 0:
-          print("Tried {} passwords: running time = {} seconds".format(count, time.time() - stime))
-          print("\tMost recent pass {}".format(pw))
+        self.pw_gen = pw_generator
+        self.multiProc = False
 
 
-  def extractServerAuthData(self, data):
-    m = auth_server_re.search(data)
-    if m is None:
-      raise AuthContextException("Failed to find server authorization request")
+    def setToMultiProc(self):
+        self.multiProc = True
 
-    self.realm = m.group('realm')
-    self.server_nonce = m.group('snonce')
-    self.hashtype = m.group('hashtype')
-    self.qop = m.group('qop')
+    def crackPassword(self, data=None, filename=None):
+        if data is None:
+            if filename is None:
+                raise AuthContextException("No data provided and filename is None")
+            else:
+                with open(filename, 'r') as f:
+                    data = f.read()
+
+        self.extractServerAuthData(data)
+        self.extractClientAuthData(data)
+
+        #print(str(self))
+        print("Starting password crack...")
+
+        # Using the notation from the wikipedia page here:
+        # http://en.wikipedia.org/wiki/Digest_access_authentication
+
+        ha1_template = "{0.username}:{0.realm}:{{}}".format(self)
+        ha2 = hashlib.md5("{0.request_method}:{0.request_uri}".format(self)).hexdigest()
+
+        # HA1:snonce:nc:cnonce:qop:HA2
+        resp = "{{}}:{0.server_nonce}:{0.nonce_count}:{0.client_nonce}:{0.qop}:{1}".format(self, ha2)
+        print("HA1 template = " + ha1_template)
+        print("HA2 = " + ha2)
+        print("resp template = " + resp)
+
+        # This is code from the old brute force method where we would just cruise through all
+        # various permutation.
+        #ascii_list = []
+        #for i in self.charset:
+        #  ascii_list.append(str(chr(i)))
+
+        count = 0
+        stime = time.time()
+        for pw in self.pw_gen:
+            ha1 = hashlib.md5(ha1_template.format(pw)).hexdigest()
+            full_resp = resp.format(ha1)
+            if hashlib.md5(full_resp).hexdigest() == self.client_resp:
+                print("Found password: {}".format(pw))
+                with open(cracked_file, 'w+') as f:
+                    f.write(pw)
+                return
+
+            count += 1
+            if count % 50000 == 0:
+                if self.multiProc:
+                    print("On Pass: {}".format(pw))
+                else:
+                    print("Tried {} passwords: running time = {} seconds".format(count, time.time() - stime))
+                    print("\tMost recent pass {}".format(pw))
 
 
-  def extractClientAuthData(self, data):
-    m = auth_resp_re.search(data)
-    if m is None:
-      raise AuthContextException("Failed to find client authorization response")
+    def extractServerAuthData(self, data):
+        m = auth_server_re.search(data)
+        if m is None:
+            raise AuthContextException("Failed to find server authorization request")
 
-    if self.server_nonce == "":
-      raise AuthContextException("Must extract server auth data first")
-
-    self.username = m.group('username')
-
-    temp = m.group('realm')
-    if temp != self.realm:
-      raise AuthContextExcpetion("Server/Client realm mismatch")
-
-    temp = m.group('snonce')
-    if temp != self.server_nonce:
-      raise AuthContextException("Server/Client nonce mismatch")
-
-    self.request_uri = m.group('uri')
-    self.client_nonce = m.group('cnonce')
-    self.nonce_count = m.group('ncount')
-
-    temp = m.group('qop')
-    if temp != self.qop:
-      raise AuthContextException("Server/Client using different qop")
+        self.realm = m.group('realm')
+        self.server_nonce = m.group('snonce')
+        self.hashtype = m.group('hashtype')
+        self.qop = m.group('qop')
 
 
-    self.client_resp = m.group('cresp')
+    def extractClientAuthData(self, data):
+        m = auth_resp_re.search(data)
+        if m is None:
+            raise AuthContextException("Failed to find client authorization response")
 
-    temp = m.group('hashtype')
-    if temp != self.hashtype:
-      raise AuthContextException("Server/Client using different hash type ({}:{})".format(self.hashtype, temp))
+        if self.server_nonce == "":
+            raise AuthContextException("Must extract server auth data first")
+
+        self.username = m.group('username')
+
+        temp = m.group('realm')
+        if temp != self.realm:
+            raise AuthContextException("Server/Client realm mismatch")
+
+        temp = m.group('snonce')
+        if temp != self.server_nonce:
+            raise AuthContextException("Server/Client nonce mismatch")
+
+        self.request_uri = m.group('uri')
+        self.client_nonce = m.group('cnonce')
+        self.nonce_count = m.group('ncount')
+
+        temp = m.group('qop')
+        if temp != self.qop:
+            raise AuthContextException("Server/Client using different qop")
 
 
-  def __str__(self):
-    repr = "realm = {0.realm}\n".format(self)
-    repr += "username = {0.username}\n".format(self)
-    repr += "password = {0.password}\n".format(self)
-    repr += "qop = {0.qop}\n".format(self)
-    repr += "server_nonce = {0.server_nonce}\n".format(self)
-    repr += "client_nonce = {0.client_nonce}\n".format(self)
-    repr += "nonce_count = {0.nonce_count}\n".format(self)
-    repr += "method = {0.request_method}\n".format(self)
-    repr += "uri = {0.request_uri}\n".format(self)
-    repr += "hash = {0.hashtype}\n".format(self)
+        self.client_resp = m.group('cresp')
 
-    return repr
+        temp = m.group('hashtype')
+        if temp != self.hashtype:
+            raise AuthContextException("Server/Client using different hash type ({}:{})".format(self.hashtype, temp))
+
+
+    def __str__(self):
+        repr = "realm = {0.realm}\n".format(self)
+        repr += "username = {0.username}\n".format(self)
+        repr += "password = {0.password}\n".format(self)
+        repr += "qop = {0.qop}\n".format(self)
+        repr += "server_nonce = {0.server_nonce}\n".format(self)
+        repr += "client_nonce = {0.client_nonce}\n".format(self)
+        repr += "nonce_count = {0.nonce_count}\n".format(self)
+        repr += "method = {0.request_method}\n".format(self)
+        repr += "uri = {0.request_uri}\n".format(self)
+        repr += "hash = {0.hashtype}\n".format(self)
+
+        return repr
 
 
 def usage(name):
     print("Usage: {} [tcpdump_output] [dictionary]".format(name))
     print("Note: This is not the raw packet data but the result of running 'tcpdump -A [packets]'")
 
+
+def startCrackProcess(auth_ctx, auth_data, shared):
+    auth_ctx.crackPassword(auth_data)
+
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         usage(sys.argv[0])
         sys.exit(1)
 
@@ -178,20 +204,53 @@ if __name__ == '__main__':
         data = f.read()
 
     pw_dict = []
-    stime = time.time()
-    with open(sys.argv[2], 'r') as f:
-        print("Reading passwords from {}".format(sys.argv[2]))
+    if len(sys.argv) == 3:
+        stime = time.time()
+        with open(sys.argv[2], 'r') as f:
+            print("Reading passwords from {}".format(sys.argv[2]))
 
-        gc.disable()
-        for line in f:
-          pw_dict.append(line)
+            gc.disable()
+            for line in f:
+                pw_dict.append(line)
 
-        gc.enable()
+            gc.enable()
 
-        print("Loaded {} from dictionary in {} seconds".format(len(pw_dict), time.time() - stime))
-        time.sleep(5)
+            print("Loaded {} from dictionary in {} seconds".format(len(pw_dict), time.time() - stime))
 
-    #print("Formatted tcpdump data:\n{}".format(data))
+    auth_context = None
+    if len(pw_dict) == 0:
+        chars = numbers + cap_letters + low_letters
+        charset = []
+        for char in chars:
+            charset.append(str(chr(char)))
 
-    auth_context = AuthContext(pw_dict)
-    auth_context.crackPassword(data)
+        # Now we create a set of processes to make the brute forcing more efficient
+        len_range = pass_max - pass_min + 1
+        len_range = len_range / max_procs
+
+        processes = []
+        proc_ranges = range(pass_min,pass_max,len_range)
+        for index,low in enumerate(proc_ranges):
+            ctx = None
+            try:
+                print("Process: pw length {} -> {}".format(low, proc_ranges[index+1]-1))
+                ctx = AuthContext(genPasswords(low, proc_ranges[index+1]-1, charset))
+            except IndexError:
+                print("Process: pw length {} -> {}".format(low, pass_max))
+                ctx = AuthContext(genPasswords(low, pass_max, charset))
+
+            ctx.setToMultiProc()
+            p = Process(target=startCrackProcess, args=(ctx, data, None))
+            p.start()
+            processes.append(p)
+
+        # Wait for them all to finish
+        for proc in processes:
+            proc.join()
+            print("Cracker process finished")
+
+    else:
+        auth_context = AuthContext(pw_dict)
+        auth_context.crackPassword(data)
+
+    print("Check {} for any cracked passwords".format(cracked_file))
